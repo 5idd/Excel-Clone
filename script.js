719b9a4e2e4a0f0c26b6a76a43682637402d1327
Siddharth <sid.saurav11@gmail.com>
2021-07-07 12:18:30 +0530

    add scroll bar, multiple cell selection and make style icon functional

--- script.js
+++ script.js
@@ -26,12 +26,126 @@ for (let i = 1; i <= 100; i++) {
 
 
 for (let i = 1; i <= 100; i++) {
-    let toAppendRow = '<div class="cell-row rowId-'+i+'"></div>';
+    let toAppendRow = '<div class="cell-row rowId-' + i + '"></div>';
     let onAppend = $(".input-cell-container");
     onAppend.append(toAppendRow);
     for (let j = 1; j <= 100; j++) {
-        let toAppend = '<div class="input-cell" id="row-' + i + '-col-' + j + '" contenteditable></div>'
-        let onAppend = $(".cell-row.rowId-"+i); //not ".cell-row .rowId-"+i ->space will not be there.
+        let toAppend = '<div class="input-cell" id="row-' + i + '-col-' + j + '"></div>'
+        let onAppend = $(".cell-row.rowId-" + i); //not ".cell-row .rowId-"+i ->space will not be there.
         onAppend.append(toAppend);
     }
 }
+
+
+//selecting a align icon
+$(".align-icon").click(function () { //yaha arrow function kaam ni kr rha...
+    $(".align-icon.selected").removeClass("selected");
+    $(this).addClass("selected");
+})
+
+$(".style-icon").click(function () { //yaha arrow function kaam ni kr rha...
+    $(this).toggleClass("selected");
+})
+
+$(".input-cell").click(function (e) { //yaha arrow function kaam ni kr rha...
+    if (e.ctrlKey) {
+        let x = getRowCol(this);
+        $(this).addClass("selected");
+        if (x[0] > 1) {
+            let topCellSelected = $('#row-' + (parseInt(x[0]) - 1) + '-col-' + parseInt(x[1])).hasClass("selected");
+            if (topCellSelected) {
+                console.log("top");
+                $(this).addClass("top-cell-selected");
+                $('#row-' + (parseInt(x[0]) - 1) + '-col-' + parseInt(x[1])).addClass("bottom-cell-selected");
+            }
+        }
+        if (x[1] > 1) {
+            let leftCellSelected = $('#row-' + parseInt(x[0]) + '-col-' + (parseInt(x[1]) - 1)).hasClass("selected");
+            if (leftCellSelected) {
+                console.log("left");
+                $(this).addClass("left-cell-selected");
+                $('#row-' + parseInt(x[0]) + '-col-' + (parseInt(x[1]) - 1)).addClass("right-cell-selected");
+            }
+        }
+
+        if (x[1] < 100) {
+            let rightCellSelected = $('#row-' + parseInt(x[0]) + '-col-' + (parseInt(x[1]) + 1)).hasClass("selected");
+            if (rightCellSelected) {
+                console.log("right");
+                $(this).addClass("right-cell-selected");
+                $('#row-' + parseInt(x[0]) + '-col-' + (parseInt(x[1]) + 1)).addClass("left-cell-selected");
+            }
+        }
+
+        if (x[0] < 100) {
+            let bottomCellSelected = $('#row-' + (parseInt(x[0]) + 1) + '-col-' + parseInt(x[1])).hasClass("selected");
+            if (bottomCellSelected) {
+                console.log("bottom");
+                $(this).addClass("bottom-cell-selected");
+                $('#row-' + (parseInt(x[0]) + 1) + '-col-' + parseInt(x[1])).addClass("top-cell-selected");
+            }
+        }
+    }
+    else {
+        console.log("hi");
+        $(".input-cell.selected").removeClass("selected");
+        $(this).addClass("selected");
+    }
+})
+
+$(".input-cell").dblclick(function () {
+    $(".input-cell.selected").removeClass("selected");
+    $(this).attr("contenteditable", "true");
+    $(this).focus();
+})
+
+$(".input-cell").blur(function(){
+    $(".input-cell.selected").attr("contenteditable", "false"); //focus hatne p contenteditable false ho jae...
+})
+
+$(".input-cell-container").scroll(function () {
+    $(".column-name-container").scrollLeft(this.scrollLeft) // jitna input cell scroll hua utna column name container bhi scroll kr do.
+    $(".row-name-container").scrollTop(this.scrollTop)
+})
+
+
+function getRowCol(ele) {
+    let idArray = $(ele).attr("id").split("-");
+    let rowId = idArray[1];
+    let colId = idArray[3];
+    return [rowId, colId];
+}
+
+//style-icons--------------------------------------------------------------------
+
+function updateCell(property, value){ //to make cell bold italic underline...
+    $(".input-cell.selected").each(function(){
+      $(this).css(property, value);  
+    })
+}
+
+$(".icon-bold").click(function(){
+    if($(this).hasClass("selected")){
+        updateCell("font-weight", "bold");
+    }else{
+        updateCell("font-weight", ""); // to remove bold ----> empty string
+    }
+})
+
+$(".icon-italic").click(function(){
+    if($(this).hasClass("selected")){
+        updateCell("font-style", "italic");
+    }else{
+        updateCell("font-style", ""); 
+    }
+})
+
+$(".icon-underline").click(function(){
+    if($(this).hasClass("selected")){
+        updateCell("text-decoration", "underline");
+    }else{
+        updateCell("text-decoration", ""); 
+    }
+})
+
+//----------------------------------------------------------------------------
